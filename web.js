cd63d21935f3c5086cf90d2b61191961b5863980
seislin0920 <seislin0920@gmail.com>
2023-04-11 17:33:12 +0800

    JQuery

--- web.js
+++ web.js
@@ -1,24 +1,25 @@
 //Blog
-let title = document.getElementById('title');
-let content = document.getElementById('content');
+// let title = document.getElementById('title');
+// let content = document.getElementById('content');
 
-let bt = document.getElementById('bt');
+// let bt = document.getElementById('bt');
 
-let list = document.getElementById('list');
+// let list = document.getElementById('list');
 
-bt.addEventListener("click", function() {
-    list.innerHTML = list.innerHTML + `
-    <div class="article">
-            <h2>${title.value}</h2>
-            <p>${content.value}</p>
-            <hr/>
-        </div>
-    `;//``可以任意換行與引用js
-    title.value = '';
-    content.value = '';
-});
+// bt.addEventListener("click", function() {
+//     list.innerHTML = list.innerHTML + `
+//     <div class="article">
+//             <h2>${title.value}</h2>
+//             <p>${content.value}</p>
+//             <hr/>
+//         </div>
+//     `;//``可以任意換行與引用js
+//     title.value = '';
+//     content.value = '';
+// });
 
 //jQuery
+
 //方法一
 // let btn = document.querySelector('#bt');
 // btn.addEventListener('click', function() {
@@ -42,19 +43,46 @@ bt.addEventListener("click", function() {
 //         alert('click!');
 //   });
 // };
-// getclick('#bt');
+// getclick('#bt'); //alert('click!')爲封閉之動作
 
 //方法四
-function get(elem) {
+// function $(elem) {
+//     let btn = document.querySelector(elem);
+//     return {
+//         aEL:function(act, func) {
+//           btn.addEventListener(act, func);
+//         } //物件方式引入act function，這裏的"func"爲addEventListener內可填之參數
+//       };
+// };
+
+// $('#bt').aEL("mouseover",function () {
+//     alert('go away!');
+//   });
+
+//融合！！
+function $(elem) {
     let btn = document.querySelector(elem);
     return {
-        act: function(func) {
-          btn.addEventListener('click', func);
-        }
-      }
+        aEL:function(act, func) {
+          btn.addEventListener(act, func);
+        }, //物件方式引入act function，這裏的"func"爲addEventListener內可填之參數
+      };
 };
 
-get('#bt').act(function () {
-    alert('click!');
-  })
-
+let title = document.querySelector('#title');
+let content = document.querySelector('#content');
+$('#list').aEL('click',function () {
+    $('#list').innerHTML = $('#list').innerHTML + `
+      <div class="article">
+              <h2>${title.value}</h2>
+              <p>${content.value}</p>
+              <hr/>
+          </div>
+      `;  //``可以任意換行與引用js
+      title.value = '';
+      content.value = '';
+});
+  
+$('#bt').aEL('click',function () {
+    alert('輸入成功');
+  });
